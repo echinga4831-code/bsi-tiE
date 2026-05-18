3522fa35e0b8643f5c2075311971e5bf2d52180c
echinga4831-code <echinga4831@utm.edu.ec>
2026-05-17 23:36:51 -0500

    Update settings.py

--- dsi_ti/settings.py
+++ dsi_ti/settings.py
@@ -76,15 +76,17 @@ WSGI_APPLICATION = 'dsi_ti.wsgi.application'
 # https://docs.djangoproject.com/en/6.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql",
-        "NAME": "bsi_ti",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432",
-    }
+
+'default': {
+
+'ENGINE': 'django.db.backends.sqlite3",
+
+'NAME': BASE_DIR / 'db.sqlite3',
+
 }
+
+}
+
 
 
 # Password validation
